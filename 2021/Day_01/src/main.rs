023de2d38ab0702cc7db50d04f375a0a8d57b8b2
Simon Randby <simon.randby@gmail.com>
2021-12-30 20:03:43 +0100

    Day 1: Read input from file

--- 2021/Day_01/src/main.rs
+++ 2021/Day_01/src/main.rs
@@ -1,18 +1,15 @@
 mod count_increases_from_previous_value;
 
 fn main(){
-    let example_values: Vec<u64> = vec![
-        199,
-        200,
-        208,
-        210,
-        200,
-        207,
-        240,
-        269,
-        260,
-        263
-    ];
-    let count = count_increases_from_previous_value::using_simple_for_loop(&example_values);
-    println!("Count is: \"{}\"", count);
+    let values = read_input_from_file("./src/input.txt");
+    let count = count_increases_from_previous_value::using_simple_for_loop(&values);
+    println!("Day 1: Sonar Sweep results");
+    println!("I have found that \"{}\" measurements are larger than the previous measurement", count);
+}
+
+fn read_input_from_file(filename: &str) -> Vec<u64> {
+    let contents = std::fs::read_to_string(filename).expect("Failed to read from file");
+    let mut lines = contents.lines();
+    let numbers: Vec<u64> = lines.map(|s| s.parse().expect("Failed to parse line")).collect();
+    return numbers;
 }
