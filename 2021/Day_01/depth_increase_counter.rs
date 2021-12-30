104ef4f6f6bb7e0b385e2d8e7b75cdeaa8c5e315
Simon Randby <simon.randby@gmail.com>
2021-12-30 18:15:20 +0100

    Day 1: Fix weird formatting

--- 2021/Day_01/depth_increase_counter.rs
+++ 2021/Day_01/depth_increase_counter.rs
@@ -1,4 +1,3 @@
-
 fn main(){
     println!("hello world!!");
     let example_values: Vec<u64> = vec![
@@ -13,25 +12,25 @@ fn main(){
         260,
         263
         ];
-        let count = count_increases_from_previous_value(&example_values);
-        println!("Count is: \"{}\"", count);
-        println!("Running second thingy.");
-        let c2 = count_increases_using_window_slices(&example_values);
-        println!("Count2 is: \"{}\"", c2);
-    }
+    let count = count_increases_from_previous_value(&example_values);
+    println!("Count is: \"{}\"", count);
+    println!("Running second thingy.");
+    let c2 = count_increases_using_window_slices(&example_values);
+    println!("Count2 is: \"{}\"", c2);
+}
 
-    fn count_increases_from_previous_value(values: &Vec<u64>) -> u64 {
-        let mut number_of_increases: u64 = 0;
-        use std::convert::TryInto;
-        let number_of_values = values.len().try_into().unwrap();
-        // Skip first element by iterating from 1 instead of 0.
-        for n in 1..number_of_values {
-            if values[n] > values[n-1] {
-                number_of_increases += 1;
-            }
+fn count_increases_from_previous_value(values: &Vec<u64>) -> u64 {
+    let mut number_of_increases: u64 = 0;
+    use std::convert::TryInto;
+    let number_of_values = values.len().try_into().unwrap();
+    // Skip first element by iterating from 1 instead of 0.
+    for n in 1..number_of_values {
+        if values[n] > values[n-1] {
+            number_of_increases += 1;
         }
-        return number_of_increases;
     }
+    return number_of_increases;
+}
 
 fn count_increases_using_window_slices(values: &Vec<u64>) -> u64 {
     // Trying out how windows work.
