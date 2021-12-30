3a53a864358e5bcbf51ebcd80c1b067d355544ae
Simon Randby <simon.randby@gmail.com>
2021-12-30 20:08:17 +0100

    Write note about why no hughge oneliner

--- 2021/Day_01/src/main.rs
+++ 2021/Day_01/src/main.rs
@@ -8,6 +8,10 @@ fn main(){
 }
 
 fn read_input_from_file(filename: &str) -> Vec<u64> {
+    // Could save some resources by simplifying this to a hughe one-liner,
+    // but I figure that the way it stands now
+    // it is easier for me to reuse parts when I learn more later.
+
     let contents = std::fs::read_to_string(filename).expect("Failed to read from file");
     let lines = contents.lines();
     let numbers: Vec<u64> = lines.map(|s| s.parse().expect("Failed to parse line")).collect();
