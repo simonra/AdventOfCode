f4398516bad61172bc29bc44f645cbdb36b3bd74
Simon Randby <simon.randby@gmail.com>
2021-12-31 12:44:33 +0100

    Update main to print new results

--- 2021/Day_01/src/main.rs
+++ 2021/Day_01/src/main.rs
@@ -2,10 +2,13 @@ mod count_increases_from_previous_value;
 mod sliding_window_count_increases;
 
 fn main(){
+    println!("Day 1: Sonar Sweep results");
     let values = read_input_from_file("./src/input.txt");
     let count = count_increases_from_previous_value::using_simple_for_loop(&values);
-    println!("Day 1: Sonar Sweep results");
     println!("I have found that \"{}\" measurements are larger than the previous measurement", count);
+    let sliding_window_count = sliding_window_count_increases::using_window_slices(&values);
+    println!("Using a 3 measurement sliding window I found \"{}\" sums to be larger than the previous.", sliding_window_count);
+
 }
 
 fn read_input_from_file(filename: &str) -> Vec<u64> {
