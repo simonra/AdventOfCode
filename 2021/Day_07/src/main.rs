ae64b09f93c78fbb8244624610e98916f6ae574c
Simon Randby <simon.randby@gmail.com>
2022-01-05 22:47:59 +0100

    Note some ideas for day 7

---
+++ 2021/Day_07/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+    /*
+    Ideas:
+    Brute force
+    Binary search
+    average
+    median
+    start binary search at average
+    freshen up on center of gravity
+    */
+}
