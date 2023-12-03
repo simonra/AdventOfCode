d36394c18a417ddc4bd77347642f22771afac01c
Simon Randby <simon.randby@gmail.com>
2023-12-03 13:27:07 +0100

    Advent of code 2023: Day 3 sample input working

---
+++ 2023/Day_03/cs/Day03/Program.cs
@@ -0,0 +1,211 @@
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+using ILoggerFactory factory = LoggerFactory.Create(
+    builder =>
+    {
+        builder.SetMinimumLevel(LogLevel.Information);
+        builder.SetMinimumLevel(LogLevel.Debug);
+        builder.AddConsole();
+    });
+ILogger logger = factory.CreateLogger("Day03");
+var serializerOptions = new JsonSerializerOptions { WriteIndented = true };
+
+var part1SampleDataFilePath = "sample_input-part_1.txt";
+var part1SampleDataExpectedResult = 4361;
+
+uint sum = 0;
+// string previousLine = string.Empty;
+var previousSchematicLine = new EngineSchematicLine()
+{
+    LineNumber = uint.MaxValue,
+    PartNumbers = new List<PartNumber>(),
+    Symbols = new List<Symbol>(),
+};
+uint lineNumber = 0;
+var lines = File.ReadLines(part1SampleDataFilePath);
+var uncountedPartNumbersPrevious = new List<PartNumber>();
+var uncountedPartNumbersCurrent = new List<PartNumber>();
+foreach (var line in lines)
+{
+    var currentSchematicLine = ParseEngineSchematicLine(line, lineNumber);
+    logger.LogDebug($"Parsing line {line} yields {JsonSerializer.Serialize(currentSchematicLine, serializerOptions)}");
+    uncountedPartNumbersCurrent.AddRange(currentSchematicLine.PartNumbers);
+    foreach (var symbol in currentSchematicLine.Symbols)
+    {
+        for (int i = uncountedPartNumbersPrevious.Count - 1; i >= 0; i--)
+        {
+            if (IsAdjacent(symbol, uncountedPartNumbersPrevious[i]))
+            {
+                sum += uncountedPartNumbersPrevious[i].Value;
+                uncountedPartNumbersPrevious.RemoveAt(i);
+            }
+        }
+        for (int i = uncountedPartNumbersCurrent.Count - 1; i >= 0; i--)
+        {
+            if (IsAdjacent(symbol, uncountedPartNumbersCurrent[i]))
+            {
+                sum += uncountedPartNumbersCurrent[i].Value;
+                uncountedPartNumbersCurrent.RemoveAt(i);
+            }
+        }
+        // if(!uncountedPartNumbersPrevious.Any())
+        // {
+        //     break;
+        // }
+    }
+    if (uncountedPartNumbersCurrent.Count > 0)
+    {
+        for (int i = uncountedPartNumbersCurrent.Count - 1; i >= 0; i--)
+        {
+            foreach (var symbol in previousSchematicLine.Symbols)
+            {
+                if (IsAdjacent(symbol, uncountedPartNumbersCurrent[i]))
+                {
+                    sum += uncountedPartNumbersCurrent[i].Value;
+                    uncountedPartNumbersCurrent.RemoveAt(i);
+                    if (uncountedPartNumbersCurrent.Count == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+    // ToDo: The actual work
+    // For each symbol in current
+    //    check adjacency to number in current and previous
+    // For each number
+    //    check adjacency to symbol in previous
+    previousSchematicLine = currentSchematicLine;
+    uncountedPartNumbersPrevious.Clear();
+    uncountedPartNumbersPrevious.AddRange(uncountedPartNumbersCurrent);
+    uncountedPartNumbersCurrent.Clear();
+    lineNumber++;
+}
+
+logger.LogInformation($"Sum is {sum}, expected {part1SampleDataExpectedResult}");
+
+// Checks if number is adjacent on line above, below, or current
+bool IsAdjacent(Symbol symbol, PartNumber partNumber)
+{
+    if (partNumber.OffsetBegin <= symbol.Offset && symbol.Offset <= partNumber.OffsetEnd)
+    {
+        return true;
+    }
+
+    if (symbol.Offset + 1 == partNumber.OffsetBegin)
+    {
+        return true;
+    }
+
+    if (symbol.Offset == partNumber.OffsetEnd + 1)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+EngineSchematicLine ParseEngineSchematicLine(string input, uint lineNumber)
+{
+    var partNumbers = new List<PartNumber>();
+    var symbols = new List<Symbol>();
+
+    bool previousIsDigit = false;
+    string currentNumberUnparsed = string.Empty;
+    uint currentNumberFirstOffset = 0;
+    // uint currentNumberLastOffset = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (Char.IsDigit(input[i]))
+        {
+            // ToDo
+            if (previousIsDigit)
+            {
+                currentNumberUnparsed += input[i];
+            }
+            else
+            {
+                currentNumberUnparsed = $"{input[i]}";
+                currentNumberFirstOffset = (uint)i;
+                previousIsDigit = true;
+            }
+        }
+        else
+        {
+            if (previousIsDigit)
+            {
+                // currentNumberLastOffset = i - 1;
+                partNumbers.Add(
+                    new PartNumber()
+                    {
+                        Value = UInt16.Parse(currentNumberUnparsed),
+                        OffsetBegin = currentNumberFirstOffset,
+                        OffsetEnd = (uint)i - 1,
+                        // OffsetEnd = currentNumberLastOffset
+                    });
+            }
+            if (input[i] != '.')
+            {
+                symbols.Add(new Symbol() { Value = input[i], Offset = (uint)i });
+            }
+            previousIsDigit = false;
+        }
+        // if (input[i] != '.')
+        // {
+        //     // ToDo
+        // }
+        // if (!Char.IsDigit(input[i]) && input[i] != '.')
+        // {
+        //     // Handle symbol;
+        // }
+    }
+
+    var result = new EngineSchematicLine()
+    {
+        LineNumber = lineNumber,
+        PartNumbers = partNumbers,
+        Symbols = symbols,
+    };
+    return result;
+}
+
+
+// bool IsSymbol(char c)
+// {
+//     return !Char.IsDigit(c) && c != '.';
+// }
+
+// line
+// {
+//     numbers [
+//         {value, offsetStart, offsetEnd},
+//         { value, offsetStart, offsetEnd}
+//     ]
+//     symbols[
+//         { offset, (value)}
+//     ]
+// }
+
+public record EngineSchematicLine
+{
+    public required uint LineNumber { get; init; }
+    public required List<PartNumber> PartNumbers { get; init; }
+    public required List<Symbol> Symbols { get; init; }
+}
+
+public record PartNumber
+{
+    public required uint Value { get; init; }
+    public required uint OffsetBegin { get; init; }
+    public required uint OffsetEnd { get; init; }
+}
+
+public record Symbol
+{
+    public required char Value { get; init; }
+    public required uint Offset { get; init; }
+}
