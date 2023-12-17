78678a0e88d4d26d34ac284da026ecf8185ade8c
Simon Randby <simon.randby@gmail.com>
2023-12-17 11:35:12 +0100

    AoC 2023 Day 14: Part 2 WiP Add the different tilts

--- 2023/Day_14/cs/Day14/Program.cs
+++ 2023/Day_14/cs/Day14/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -15,6 +15,7 @@ ILogger logger = factory.CreateLogger("Day14");
 
 TestPart1OnSampleInput();
 RunPart1();
+TestPart2OnSampleInput();
 
 void RunPart1()
 {
@@ -31,7 +32,7 @@ void TestPart1OnSampleInput()
     var inputFile = "sample_input-part_1.txt";
 
     var tiltedNorth = inputFile.ParseMap().TiltNorth();
-    logger.LogInformation($"System tilted north is\n{tiltedNorth.ToJson(false).Replace("]","\n]")}");
+    logger.LogInformation($"System tilted north is\n{tiltedNorth.ToJson(false).Replace("]", "\n]")}");
 
     var result = inputFile.ParseMap().TiltNorth().TotalLoad();
     uint expectedResult = 136;
@@ -44,6 +45,44 @@ void TestPart1OnSampleInput()
         logger.LogInformation("Testing Part 1: Processing sample data yielded expected result!");
     }
 }
+
+
+void TestPart2OnSampleInput()
+{
+    logger.LogInformation($"Starting {nameof(TestPart2OnSampleInput)}");
+    var inputFile = "sample_input-part_1.txt";
+
+    var tiltedNorth = inputFile.ParseMap().TiltNorth();
+    logger.LogInformation($"System tilted north is\n{tiltedNorth.ToJson(false).Replace("]", "\n]")}");
+
+    var tiltedWest = inputFile.ParseMap().TiltWest();
+    logger.LogInformation($"System tilted west is\n{tiltedWest.ToJson(false).Replace("]", "\n]")}");
+
+    var tiltedSouth = inputFile.ParseMap().TiltSouth();
+    logger.LogInformation($"System tilted south is\n{tiltedSouth.ToJson(false).Replace("]", "\n]")}");
+
+    var tiltedEast = inputFile.ParseMap().TiltEast();
+    logger.LogInformation($"System tilted east is\n{tiltedEast.ToJson(false).Replace("]", "\n]")}");
+
+    var result = inputFile.ParseMap().TiltNorth().TotalLoad();
+    uint expectedResult = 64;
+    if (result != expectedResult)
+    {
+        logger.LogError($"Testing part 2 on sample data failed. Got {result}, expected {expectedResult}");
+    }
+    else
+    {
+        logger.LogInformation("Testing Part 2: Processing sample data yielded expected result!");
+    }
+}
+
+// TestRotateCounterclockwise();
+// void TestRotateCounterclockwise()
+// {
+//     var inputFile = "sample_input-part_1.txt";
+//     var rotated = inputFile.ParseMap().RotateCounterclockwise();
+//     logger.LogInformation($"Rotated ccw is\n{rotated.ToJson(false).Replace("]","]\n")}");
+// }
 
 // TestIndexOfApproaches();
 // void TestIndexOfApproaches()
@@ -74,7 +113,7 @@ static class ExtensionMethods
             while ((line = sr.ReadLine()) != null)
             {
                 var nextLine = new List<char>();
-                foreach(var character in line)
+                foreach (var character in line)
                 {
                     nextLine.Add(character);
                 }
@@ -86,37 +125,62 @@ static class ExtensionMethods
 
     public static List<List<char>> TiltNorth(this List<List<char>> source)
     {
-        var transposed = source.Transpose().CollectLists();
+        return source
+            .Transpose().CollectLists()
+            .PushAllLeft()
+            .Transpose().CollectLists();
+    }
+
+    public static List<List<char>> TiltWest(this List<List<char>> source)
+    {
+        return source.PushAllLeft();
+    }
+
+    public static List<List<char>> TiltSouth(this List<List<char>> source)
+    {
+        return source
+            .Transpose().CollectLists()
+            .ReverseAll()
+            .PushAllLeft()
+            .ReverseAll()
+            .Transpose().CollectLists();
+    }
+
+    public static List<List<char>> TiltEast(this List<List<char>> source)
+    {
+        return source.ReverseAll().PushAllLeft().ReverseAll();
+    }
+
+    public static List<List<char>> PushAllLeft(this List<List<char>> source)
+    {
         var sortedSystem = new List<List<char>>();
-        foreach(var row in transposed)
+        foreach (var row in source)
         {
             // Console.WriteLine($"Row before\t{row.ToJson(false)}");
-            // var sectionsBetweenFixedPositions =
             var fixedPointsInRow = row.IndexesOf('#').ToList();
             var relevantFixedPointIndexes = new List<int>();
-            if(fixedPointsInRow.Any())
+            if (fixedPointsInRow.Any())
             {
                 relevantFixedPointIndexes.AddRange(fixedPointsInRow.Where(x => !fixedPointsInRow.Any(y => y == x + 1)));
-                if(relevantFixedPointIndexes.First() != 0)  relevantFixedPointIndexes = relevantFixedPointIndexes.Prepend(-1).ToList();
-                if(relevantFixedPointIndexes.Last() != row.Count() - 1)  relevantFixedPointIndexes = relevantFixedPointIndexes.Append(row.Count()).ToList();
+                if (relevantFixedPointIndexes.First() != 0) relevantFixedPointIndexes = relevantFixedPointIndexes.Prepend(-1).ToList();
+                if (relevantFixedPointIndexes.Last() != row.Count() - 1) relevantFixedPointIndexes = relevantFixedPointIndexes.Append(row.Count()).ToList();
             }
             else
             {
                 relevantFixedPointIndexes.Add(-1);
                 relevantFixedPointIndexes.Add(row.Count());
             }
-            // var numberOfOsPerGroup = relevantFixedPointIndexes.SlidingWindow(2).Select(fixedPointPair => row.CountInRange('O', fixedPointPair.First() + 1, fixedPointPair.Skip(1).First() - 1));
 
             var rowSorted = new List<char>();
             for (int i = 0; i < row.Count(); i++)
             {
                 rowSorted.Add('.');
             }
-            foreach(var pointIndex in fixedPointsInRow)
+            foreach (var pointIndex in fixedPointsInRow)
             {
                 rowSorted[pointIndex] = '#';
             }
-            foreach(var fixedPointPair in relevantFixedPointIndexes.SlidingWindow(2))
+            foreach (var fixedPointPair in relevantFixedPointIndexes.SlidingWindow(2))
             {
                 var firstOffset = fixedPointPair.First() + 1;
                 var lastOffset = fixedPointPair.Skip(1).First();
@@ -131,7 +195,7 @@ static class ExtensionMethods
             sortedSystem.Add(rowSorted);
         }
 
-        return sortedSystem.Transpose().CollectLists();
+        return sortedSystem;
     }
 
     public static int TotalLoad(this List<List<char>> source)
@@ -163,9 +227,9 @@ static class ExtensionMethods
     public static IEnumerable<int> IndexesWhere<T>(this IEnumerable<T> source, Func<T, bool> predicate)
     {
         int i = 0;
-        foreach(var item in source)
+        foreach (var item in source)
         {
-            if(predicate(item)) yield return i;
+            if (predicate(item)) yield return i;
             i++;
         }
     }
@@ -185,7 +249,7 @@ static class ExtensionMethods
 
     public static IEnumerable<IEnumerable<T>> SlidingWindow<T>(this IEnumerable<T> source, int windowSize)
     {
-        if(source.Count() <= windowSize)
+        if (source.Count() <= windowSize)
         {
             yield return source;
         }
@@ -197,4 +261,51 @@ static class ExtensionMethods
             }
         }
     }
+
+    public static IEnumerable<IEnumerable<T>> RotateCounterclockwise<T>(this IEnumerable<IEnumerable<T>> original)
+    {
+        var originalRows = original.Count();
+        var originalColumns = original.First().Count();
+
+        // for (int i = 0; i < originalRows; i++)
+        for (int i = originalColumns - 1; i >= 0; i--)
+        {
+            var nextResult = new List<T>();
+            for (int j = 0; j < originalRows; j++)
+            {
+                nextResult.Add(original.Skip(j).First().Skip(i).First()); // original[j]]i]
+            }
+            yield return nextResult.AsEnumerable();
+        }
+    }
+
+    public static List<List<T>> ReverseAll<T>(this List<List<T>> original)
+    {
+        for (int i = 0; i < original.Count(); i++)
+        {
+            // original[i] = original[i].Reverse();
+            original[i].Reverse();
+        }
+        return original;
+    }
+
+    // public static List<List<T>> RotateCounterclockwise<T>(this List<List<T>> original)
+    // {
+    //     var originalRows = original.Count();
+    //     var originalColumns = original.First().Count();
+
+    //     var result = new List<List<T>>();
+    //     for (int i = 0; i < originalColumns; i++)
+    //     {
+    //         result.Add(new List<T>());
+    //     }
+
+    //     for (int yOriginal = 0; yOriginal < originalRows; yOriginal++)
+    //     {
+    //         for (int xOriginal = 0; xOriginal < originalColumns; xOriginal++)
+    //         {
+    //             result[originalColumns - 1 - xOriginal].Add(original[yOriginal][xOriginal]);
+    //         }
+    //     }
+    // }
 }
