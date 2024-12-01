9f4c6a809462402cc091bac62a1a3374511ca80a
Simon Randby <simon.randby@gmail.com>
2024-12-01 16:15:03 +0100

    Day 1 part 2 done!

--- 2024/Day_01/Day01/Program.fs
+++ 2024/Day_01/Day01/Program.fs
@@ -29,11 +29,68 @@ let sortedSecond = secondColumn |> List.sort
 printfn $"First column content after sorting: %A{sortedFirst |> List.ofSeq}"
 printfn $"Second column content after sorting: %A{sortedSecond}"
 
-let absoluteDiffs = (sortedFirst, sortedSecond) ||> Seq.map2(fun x y -> Math.Abs(x-y))
+let absoluteDiffs =
+    (sortedFirst, sortedSecond) ||> Seq.map2 (fun x y -> Math.Abs(x - y))
+
 printfn $"Content after diffing: %A{absoluteDiffs |> List.ofSeq}"
 
-let folded = Seq.fold (fun accumulated nextElement -> accumulated + nextElement) 0 absoluteDiffs
+let folded =
+    Seq.fold (fun accumulated nextElement -> accumulated + nextElement) 0 absoluteDiffs
+
 printfn $"Sum of diffs: %A{folded}"
+printfn $"Part 1 done"
+printfn $"Part 2 starting"
+
+let countsInSecond = sortedSecond |> Seq.countBy (fun x -> x)
+printfn $"Counts of each element in second column: %A{countsInSecond}"
+// let scoreIfPresent tupleCollection value =
+
+let countsInFirst = sortedFirst |> Seq.countBy id
+printfn $"Counts of each element in first column: %A{countsInFirst}"
+
+let countsInSecondIfInFirst =
+    countsInSecond |> Seq.where (fun pair -> Seq.contains (fst pair) firstColumn)
+printfn $"Counts of each element in second column that are present in first: %A{countsInSecondIfInFirst}"
+
+let countsInFirstIfInSecond = countsInFirst |> Seq.where (fun pair -> Seq.contains (fst pair) secondColumn)
+printfn $"Counts of each element in first column that are present in second: %A{countsInFirstIfInSecond}"
+
+let similarityScore3 = Seq.fold2 (fun accumulated firstPair secondPair -> accumulated + (fst firstPair) * (snd secondPair) * (snd firstPair)) 0 countsInFirstIfInSecond countsInSecondIfInFirst
+printfn $"Similarity score take 3 is %A{similarityScore3}"
+
+exit 0
+let similarityScore =
+    Seq.fold
+        (fun accumulated nextPair ->
+            accumulated
+            + snd nextPair
+              * (firstColumn
+                 |> Seq.tryPick (fun n -> if n = fst nextPair then Some(int n) else Some(int 0)))
+                  .Value)
+        0
+        countsInSecond
+
+printfn $"Similarity score is %A{similarityScore}"
+
+let similarityScore2 =
+    Seq.fold
+        (fun accumulated nextValue ->
+            accumulated
+            + nextValue
+              * (Seq.tryPick
+                  (fun valueCountPairFromSecondColumn ->
+                      if fst valueCountPairFromSecondColumn = nextValue then
+                          Some(int (snd valueCountPairFromSecondColumn))
+                      else
+                          Some(int 0))
+                  countsInSecond)
+                  .Value)
+        0
+        firstColumn
+
+printfn $"Similarity score take 2 is %A{similarityScore2}"
+// let similarityScore = firstColumn |> (fun x -> x * countsInSecond.)
+// let similarityScore = firstColumn |> (fun x -> x * (match x with countsInSecond when countsInSecond[0] == x else 0))
 
 // File.ReadAllLines("Input/Example.txt") |> Seq.iter (fun s1 -> printfn $"%A{s1}")
 
---
+++ 2024/README.md
@@ -0,0 +1,6 @@
+2024 is the year of F#!
+
+# Resouces
+https://learn.microsoft.com/en-us/dotnet/fsharp/tour
+https://learn.microsoft.com/en-us/dotnet/fsharp/language-reference/symbol-and-operator-reference/
+https://fsharp.github.io/fsharp-core-docs/reference
