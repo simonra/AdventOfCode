f20827b6dba57cc8d507585cec181e4d120b2343
Simon Randby <simon.randby@gmail.com>
2024-12-03 12:27:29 +0100

    Day 3: Done! Both 1 and 2

--- 2024/Day_02/Day02/Program.fs
+++ 2024/Day_02/Day02/Program.fs
@@ -11,12 +11,13 @@ let lines = seq { yield! System.IO.File.ReadLines inputFileName }
 
 type Level = int
 type Report = { Levels: Level seq }
+
 type ChangeDirection =
     | Increasing
     | Decreasing
     | Unchanging
 
-let DifferenceIsSafe (first:Level) (second:Level) : bool =
+let DifferenceIsSafe (first: Level) (second: Level) : bool =
     let difference = Math.Abs(second - first)
     1 <= difference && difference <= 3
 
@@ -27,12 +28,13 @@ let DifferenceIsSafe (first:Level) (second:Level) : bool =
 //     : bool =
 //         true
 
-let ReportIsSafe (report:Report) : bool =
+let ReportIsSafe (report: Report) : bool =
     // printfn $"Processing report %A{report}"
     let mutable allDifferencesAreSafe = true
     let mutable allValuesAreMonotonicallyChanging = true
     let mutable direction = Unchanging
     let firstPair = Seq.pairwise report.Levels |> Seq.head
+
     if fst firstPair = snd firstPair then
         direction <- Unchanging
         allValuesAreMonotonicallyChanging <- false
@@ -44,34 +46,33 @@ let ReportIsSafe (report:Report) : bool =
     // printfn $"Report has statuses: allDifferencesAreSafe: %A{allDifferencesAreSafe} allValuesAreMonotonicallyChanging %A{allValuesAreMonotonicallyChanging} direction %A{direction}"
     // printfn $"Starting pair processing"
     Seq.pairwise report.Levels
-        |> Seq.takeWhile (fun levelPair ->
-            // printfn $"Processing pair %A{levelPair}"
-            if fst levelPair = snd levelPair then
-                direction <- Unchanging
+    |> Seq.takeWhile (fun levelPair ->
+        // printfn $"Processing pair %A{levelPair}"
+        if fst levelPair = snd levelPair then
+            direction <- Unchanging
+            allValuesAreMonotonicallyChanging <- false
+        else if fst levelPair < snd levelPair then
+            if direction <> Increasing then
                 allValuesAreMonotonicallyChanging <- false
-            else if fst levelPair < snd levelPair then
-                if direction <> Increasing then
-                    allValuesAreMonotonicallyChanging <- false
-                direction <-Increasing
-            else
-                if direction <> Decreasing then
-                    allValuesAreMonotonicallyChanging <- false
-                direction <- Decreasing
-            allDifferencesAreSafe <- DifferenceIsSafe (fst levelPair) (snd levelPair)
-            // printfn $"Pair %A{levelPair} has statuses: allDifferencesAreSafe: %A{allDifferencesAreSafe} allValuesAreMonotonicallyChanging %A{allValuesAreMonotonicallyChanging} direction %A{direction}"
-            allDifferencesAreSafe && allValuesAreMonotonicallyChanging)
-        |> Seq.iter ignore
-        // |> ignore
+
+            direction <- Increasing
+        else
+            if direction <> Decreasing then
+                allValuesAreMonotonicallyChanging <- false
+
+            direction <- Decreasing
+
+        allDifferencesAreSafe <- DifferenceIsSafe (fst levelPair) (snd levelPair)
+        // printfn $"Pair %A{levelPair} has statuses: allDifferencesAreSafe: %A{allDifferencesAreSafe} allValuesAreMonotonicallyChanging %A{allValuesAreMonotonicallyChanging} direction %A{direction}"
+        allDifferencesAreSafe && allValuesAreMonotonicallyChanging)
+    |> Seq.iter ignore
+    // |> ignore
     // printfn $"Done pair processing"
 
     allDifferencesAreSafe && allValuesAreMonotonicallyChanging
 
-let ParseReportLine (line:string) : Report =
-    {
-        Levels =
-            line.Split(' ')
-            |> Seq.map int
-    }
+let ParseReportLine (line: string) : Report =
+    { Levels = line.Split(' ') |> Seq.map int }
 
 let parsedReports = Seq.map ParseReportLine lines
 printfn $"The parsed reports are %A{parsedReports |> Seq.toList}"
@@ -79,38 +80,46 @@ printfn $"The parsed reports are %A{parsedReports |> Seq.toList}"
 let reportStatuses = Seq.map ReportIsSafe parsedReports
 printfn $"The reports statuses are %A{reportStatuses |> Seq.toList}"
 
-let numberOfSafeReports = reportStatuses |> Seq.where (fun x -> x = true) |> Seq.length
+let numberOfSafeReports =
+    reportStatuses |> Seq.where (fun x -> x = true) |> Seq.length
+
 printfn $"The number of safe reports is %A{numberOfSafeReports}"
 
-let StepIsSafe (firstReading:Level) (secondReading:Level) (direction:ChangeDirection) : bool =
+let StepIsSafe (firstReading: Level) (secondReading: Level) (direction: ChangeDirection) : bool =
     let mutable safe = true
+
     if firstReading = secondReading then
         safe <- false
-    else
-        if direction = Increasing && secondReading < firstReading then
-            safe <- false
-        else if direction = Decreasing && firstReading < secondReading then
-            safe <- false
+    else if direction = Increasing && secondReading < firstReading then
+        safe <- false
+    else if direction = Decreasing && firstReading < secondReading then
+        safe <- false
+
     if not (DifferenceIsSafe firstReading secondReading) then
         safe <- false
+
     safe
 
-let hamming s1 s2 = Seq.map2((=)) s1 s2 |> Seq.sumBy(fun b -> if b then 0 else 1)
+let hamming s1 s2 =
+    Seq.map2 ((=)) s1 s2 |> Seq.sumBy (fun b -> if b then 0 else 1)
 
-let ReportIsSafeWithBadnessTolerance (report:Report) numberOfUnsafeLevelPairsAllowed : bool =
+let ReportIsSafeWithBadnessTolerance (report: Report) numberOfUnsafeLevelPairsAllowed : bool =
     printfn $"\nProcessing report %A{report}"
     let mutable reportIsSafe = true
     let mutable direction = Unchanging
     let mutable ignoredReadings = 0
     let firstTriplet = Seq.windowed 3 report.Levels |> Seq.head
+
     if firstTriplet[0] = firstTriplet[1] then
         ignoredReadings <- ignoredReadings + 1
+
         if firstTriplet[1] = firstTriplet[2] then
             reportIsSafe <- false
             direction <- Unchanging
         else
             if not (DifferenceIsSafe firstTriplet[1] firstTriplet[2]) then
                 reportIsSafe <- false
+
             if firstTriplet[1] < firstTriplet[2] then
                 direction <- Increasing
             else
@@ -120,8 +129,10 @@ let ReportIsSafeWithBadnessTolerance (report:Report) numberOfUnsafeLevelPairsAll
             direction <- Increasing
         else
             ignoredReadings <- ignoredReadings + 1
+
             if not (DifferenceIsSafe firstTriplet[1] firstTriplet[2]) then
                 reportIsSafe <- false
+
             if firstTriplet[1] = firstTriplet[2] then
                 reportIsSafe <- false
                 direction <- Unchanging
@@ -129,38 +140,43 @@ let ReportIsSafeWithBadnessTolerance (report:Report) numberOfUnsafeLevelPairsAll
                 direction <- Increasing
             else
                 direction <- Decreasing
+    else if DifferenceIsSafe firstTriplet[0] firstTriplet[1] then
+        direction <- Decreasing
     else
-        if DifferenceIsSafe firstTriplet[0] firstTriplet[1] then
-            direction <- Decreasing
+        ignoredReadings <- ignoredReadings + 1
+
+        if not (DifferenceIsSafe firstTriplet[1] firstTriplet[2]) then
+            reportIsSafe <- false
+
+        if firstTriplet[1] = firstTriplet[2] then
+            reportIsSafe <- false
+            direction <- Unchanging
+        else if firstTriplet[1] < firstTriplet[2] then
+            direction <- Increasing
         else
-            ignoredReadings <- ignoredReadings + 1
-            if not (DifferenceIsSafe firstTriplet[1] firstTriplet[2]) then
-                reportIsSafe <- false
-            if firstTriplet[1] = firstTriplet[2] then
-                reportIsSafe <- false
-                direction <- Unchanging
-            else if firstTriplet[1] < firstTriplet[2] then
-                direction <- Increasing
-            else
-                direction <- Decreasing
-    printfn $"After considering first triplet, report is safe: '%A{reportIsSafe}', direction is '%A{direction}', number of ignore readings is '%A{ignoredReadings}'"
+            direction <- Decreasing
+
+    printfn
+        $"After considering first triplet, report is safe: '%A{reportIsSafe}', direction is '%A{direction}', number of ignore readings is '%A{ignoredReadings}'"
+
     if reportIsSafe then
-    // printfn $"Report has statuses: allDifferencesAreSafe: %A{allDifferencesAreSafe} allValuesAreMonotonicallyChanging %A{allValuesAreMonotonicallyChanging} direction %A{direction}"
-    // printfn $"Starting pair processing"
+        // printfn $"Report has statuses: allDifferencesAreSafe: %A{allDifferencesAreSafe} allValuesAreMonotonicallyChanging %A{allValuesAreMonotonicallyChanging} direction %A{direction}"
+        // printfn $"Starting pair processing"
         Seq.windowed 3 report.Levels
-            |> Seq.skip 1
-            |> Seq.takeWhile (fun levelTriplet ->
-                // printfn $"Processing pair %A{levelTriplet}"
-                if not (StepIsSafe levelTriplet[1] levelTriplet[2] direction) then
-                    ignoredReadings <- ignoredReadings + 1
-                    if not (StepIsSafe levelTriplet[0] levelTriplet[2] direction) then
-                        reportIsSafe <- false
-                // printfn $"Pair %A{levelPair} has statuses: allDifferencesAreSafe: %A{allDifferencesAreSafe} allValuesAreMonotonicallyChanging %A{allValuesAreMonotonicallyChanging} direction %A{direction}"
-                reportIsSafe && ignoredReadings <= numberOfUnsafeLevelPairsAllowed)
-            |> Seq.iter ignore
-            // |> ignore
-        // printfn $"Done pair processing"
-        // ToDo: Check tail?
+        |> Seq.skip 1
+        |> Seq.takeWhile (fun levelTriplet ->
+            // printfn $"Processing pair %A{levelTriplet}"
+            if not (StepIsSafe levelTriplet[1] levelTriplet[2] direction) then
+                ignoredReadings <- ignoredReadings + 1
+
+                if not (StepIsSafe levelTriplet[0] levelTriplet[2] direction) then
+                    reportIsSafe <- false
+            // printfn $"Pair %A{levelPair} has statuses: allDifferencesAreSafe: %A{allDifferencesAreSafe} allValuesAreMonotonicallyChanging %A{allValuesAreMonotonicallyChanging} direction %A{direction}"
+            reportIsSafe && ignoredReadings <= numberOfUnsafeLevelPairsAllowed)
+        |> Seq.iter ignore
+    // |> ignore
+    // printfn $"Done pair processing"
+    // ToDo: Check tail?
 
     reportIsSafe && ignoredReadings <= numberOfUnsafeLevelPairsAllowed
 
@@ -175,21 +191,30 @@ let ReportIsSafeWithBadnessTolerance (report:Report) numberOfUnsafeLevelPairsAll
 // let numberOfSafeReportsWithTolerance = reportStatusesWithinTolerance |> Seq.where (fun reportStatus -> reportStatus = true) |> Seq.length
 // printfn $"The number of safe reports with tolerance is %A{numberOfSafeReportsWithTolerance}"
 
-let ReportIsSafeWithBadnessToleranceBruteForce (report:Report) : bool =
+let ReportIsSafeWithBadnessToleranceBruteForce (report: Report) : bool =
     let mutable reportIsSafe = ReportIsSafe report
+
     if not reportIsSafe then
-        report.Levels |> Seq.takeWhile (fun _ -> not reportIsSafe) |> Seq.iteri (fun i v ->
+        report.Levels
+        |> Seq.takeWhile (fun _ -> not reportIsSafe)
+        |> Seq.iteri (fun i v ->
             let skippedI = Seq.removeAt i report.Levels
+
             if (ReportIsSafe { Levels = skippedI }) then
-                reportIsSafe <- true
-        )
+                reportIsSafe <- true)
+
     reportIsSafe
 
-let reportStatusesWithinTolerance = Seq.map (fun report -> ReportIsSafeWithBadnessToleranceBruteForce report) parsedReports
+let reportStatusesWithinTolerance =
+    Seq.map (fun report -> ReportIsSafeWithBadnessToleranceBruteForce report) parsedReports
 
 printfn $"Compiling safety list"
 let safetyList = reportStatusesWithinTolerance |> Seq.toList
 printfn $"The reports statuses are %A{safetyList}"
 
-let numberOfSafeReportsWithTolerance = reportStatusesWithinTolerance |> Seq.where (fun reportStatus -> reportStatus = true) |> Seq.length
+let numberOfSafeReportsWithTolerance =
+    reportStatusesWithinTolerance
+    |> Seq.where (fun reportStatus -> reportStatus = true)
+    |> Seq.length
+
 printfn $"The number of safe reports with tolerance is %A{numberOfSafeReportsWithTolerance}"
---
+++ 2024/Day_03/Day03/Program.fs
@@ -0,0 +1,54 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System.Text.RegularExpressions
+
+printfn "Hello from F#"
+
+let mutable inputFileName = "Input/Example.txt"
+inputFileName <- "Input/Example2.txt"
+inputFileName <- "Input/Input.txt"
+
+let lines = seq { yield! System.IO.File.ReadLines inputFileName }
+// let rx = Regex(@"mul\(\d+,\d+\)", RegexOptions.Compiled)
+//
+// let singleText = System.IO.File.ReadAllText inputFileName
+// let found = seq {
+//     for m in rx.Matches(singleText) do
+//         yield m.Value, m.Index
+// }
+// found
+// |> Seq.iter (fun (e, idx) -> printfn "%s at %d" e idx)
+
+let muls input = seq {
+    for m in Regex(@"mul\(\d+,\d+\)", RegexOptions.Compiled).Matches(input) do
+        let arguments = m.Value.Replace("mul(", "").Replace(")", "").Split(',')
+        yield arguments, m.Value, m.Index
+}
+
+// singleText
+// |> muls
+// |> Seq.iter (fun (ars, original, position) -> printfn $"The arguments are '%A{ars}', parsed from the original %A{original}, found at position '%A{position}'")
+//
+// printfn $"Iterating through the input line by line"
+
+lines
+|> Seq.map muls
+|> Seq.collect id // Flattens the list. Probably not a good idea for this task long term, but nice for debugging!
+|> Seq.iter (fun (ars, original, position) -> printfn $"The arguments are '%A{ars}', parsed from the original %A{original}, found at position '%A{position}'")
+
+let products inputs = inputs |> Seq.map muls |> Seq.collect id |> Seq.map (fun (args, original, position) -> (int args[0]) * (int args[1]) )
+
+// printfn $"Products are '%A{products lines |> Seq.toList}'"
+
+let sumsOfProducts inputs = inputs |> Seq.fold (fun accumulated nextValue -> accumulated + nextValue) 0
+
+printfn $"Sums of products are '%A{lines |> products |> sumsOfProducts}'"
+
+let removeSectionsAfterDonts (input:string) : string =
+    let trimNewlines = input.Replace("\n", "")
+    let firstPass = Regex(@"(?<frontgroup>don't\(\)).*?(?<backgroup>do\(\))", RegexOptions.Compiled).Replace(trimNewlines, "")
+    Regex(@"(?<frontgroup>don't\(\)).*$", RegexOptions.Compiled).Replace(firstPass, "")
+
+printfn $"Don't sections removed: '%A{lines |> Seq.map removeSectionsAfterDonts}'"
+
+printfn $"Sums of products where don'ts are excluded are '%A{seq {System.IO.File.ReadAllText inputFileName } |> Seq.map removeSectionsAfterDonts |> products |> sumsOfProducts}'"
