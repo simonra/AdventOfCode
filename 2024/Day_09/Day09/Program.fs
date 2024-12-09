6a75a3706425b466133f5c8f91492f02c01eb187
Simon Randby <simon.randby@gmail.com>
2024-12-09 15:08:49 +0100

    2024 Day 9: Part 1 done

---
+++ 2024/Day_09/Day09/Program.fs
@@ -0,0 +1,76 @@
+﻿open System
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let mutable inputFileName = "Input/Example.txt"
+inputFileName <- "Input/Input.txt"
+
+let lines = seq { yield! System.IO.File.ReadLines inputFileName }
+
+/// Decompresses the string "2333133121414131402" to the array [|Some(0); Some(0); None; None; None; Some(1); Some(1); Some(1); None; None; None; Some(2); None; None; None; Some(3); Some(3); Some(3); None; Some(4); Some(4); None; Some(5); Some(5); Some(5); Some(5); None; Some(6); Some(6); Some(6); Some(6); None; Some(7); Some(7); Some(7); None; Some(8); Some(8); Some(8); Some(8); Some(9); Some(9);|]
+let decompressInput (input:string) : int option array =
+    input
+    |> Seq.map (fun (c: char) -> int (Char.ToString c))
+    |> Seq.mapi (fun i v ->
+        if i % 2 = 1 then
+            Seq.init v (fun x -> None)
+        else
+            Seq.init v (fun x -> Some(i/2)))
+    |> Seq.collect id
+    |> Seq.toArray
+
+// https://codereview.stackexchange.com/q/163713
+let swap (first: int) (second: int) (array: 'a array): 'a array =
+    let originalFirst = array.[first]
+    array.[first] <- array.[second];
+    array.[second] <- originalFirst;
+    array
+
+let firstNonePosition (input: 'a option array) : int =
+    input |> Array.findIndex (fun item -> item = None)
+let lastSomePosition (input: 'a option array) : int =
+    input |> Array.findIndexBack (fun item -> item <> None)
+
+// let swapFirstNoneWithLastSome(array: 'a option array): 'a option array =
+
+[<TailCall>]
+let rec defragFileArray (input: int option array) : int option array =
+    let firstNonePos = input |> firstNonePosition
+    let lastSomePos = input |> lastSomePosition
+    if lastSomePos < firstNonePos then
+        input
+    else
+        input |> swap firstNonePos lastSomePos |> defragFileArray
+
+let calculateChecksum (input: int option array) : int64 =
+    input
+    |> Seq.takeWhile (fun x -> x <> None)
+    |> Seq.mapi (fun i v -> (int64 i, int64 v.Value))
+    |> Seq.fold (fun state (index,value) -> state + (index * value) ) 0
+
+module Part2 =
+    let findLastSomePositions (input: int option array) : int seq =
+        raise (NotImplementedException())
+    let findFistNoneGroupOfSize (size: int) (input: int option array) : int seq =
+        raise (NotImplementedException())
+    [<TailCall>]
+    let rec defragFileArray (input: int option array) : int option array =
+        raise (NotImplementedException())
+    let calculateChecksum (input: int option array) : int64 =
+        raise (NotImplementedException())
+
+lines
+|> Seq.head
+|> Seq.map (fun (c: char) -> int (Char.ToString c))
+|> Seq.mapi (fun i v ->
+    if i % 2 = 1 then
+        Seq.init v (fun x -> None)
+    else
+        Seq.init v (fun x -> Some(i/2)))
+|> Seq.collect id
+|> Seq.toArray
+|> defragFileArray
+|> calculateChecksum
+// |> Seq.iteri (fun i v -> printfn $"Found %A{v} at index %A{i}")
+// |> Seq.iteri (fun i v -> printf $"(%A{v})")
+|> printfn "%A"
